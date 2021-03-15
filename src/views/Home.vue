b54230dcf5f914526c97c086681731fdaab28bac
macymuhia <macymuhia@gmail.com>
2021-03-15 08:35:13 +0300

    Add instructions page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="../assets/logo.png">-->
+    <Instructions />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Instructions from '@/components/Instructions.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Instructions
   }
 }
 </script>
